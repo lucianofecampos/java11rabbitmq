3a20af78ac9b9a203ab7440bbfdeaede5686aa04
lucianocampos <luciano.fecampos@gmail.com>
2020-06-20 23:30:20 -0300

    initial commit

---
+++ src/main/java/br/com/htecweb/java11rabbitmq/Java11rabbitmqApplication.java
@@ -0,0 +1,13 @@
+package br.com.htecweb.java11rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java11rabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java11rabbitmqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/htecweb/java11rabbitmq/Java11rabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.htecweb.java11rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java11rabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
